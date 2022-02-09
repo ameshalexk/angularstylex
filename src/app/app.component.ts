53153fa93366d21afe9e6cc8a0635bdb2bd31fc3
ameshalexk <ameshalexusa@gmail.com>
2022-02-09 13:09:44 -0600

    pizza ex done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PipeTransform, Pipe } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -11,19 +12,56 @@ export class AppComponent {
   public font: number = 20;
   public bold: boolean = false;
   public italics: boolean = false;
+  public displayHello: boolean = false;
+  public isLoggedIn: boolean = true;
+  public animal: string = 'tiger';
+  public list = ["chair", "table", "lamp"];
+  public total = 15;
 
   public fontStyles: object = {
-    color: this.color,
     fontSize: this.font,
-    bold: this.bold
   }
   toggleBold(): void {
-    console.log(this.bold)
     this.bold = !this.bold;
+    this.displayHello = !this.displayHello;
+
   }
   toggleItalics(): void {
-    console.log(this.italics)
     this.italics = !this.italics;
   }
+
+  public toppings: any = {
+    pepperoni: 0.50,
+    sausage: 1.50,
+    peppers: 0.50,
+    onions: 0.50,
+    mushrooms: 0.75,
+    pineapple: 1.00,
+    bacon: 1.00,
+    ham: 0.75,
+    spinach: 0.75,
+    cheese: 0,
+    sauce: 0,
+  }
+
+
+  public toppingsList = Object.keys(this.toppings);
+  public toppingsListVal = Object.values(this.toppings);
+
+  public selection = [] as any;
+  public price = [] as any;
+
+  changeSelection(val: string) {
+    if (!this.selection[val] && this.selection.indexOf(val) === -1) {
+      this.selection.push(val);
+      this.total = this.total + this.toppings[val];
+
+    } else {
+      this.selection.pop(val);
+      this.total = this.total - this.toppings[val];
+
+    }
+  }
+
 
 }
