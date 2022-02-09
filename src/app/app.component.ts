f17b586f9027a5c983b3fc1676df36da4a68fd66
ameshalexk <ameshalexusa@gmail.com>
2022-02-08 20:11:09 -0600

    ex complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'stylex';
+
+  public color: string = "";
+  public font: number = 20;
+  public bold: boolean = false;
+  public italics: boolean = false;
+
+  public fontStyles: object = {
+    color: this.color,
+    fontSize: this.font,
+    bold: this.bold
+  }
+  toggleBold(): void {
+    console.log(this.bold)
+    this.bold = !this.bold;
+  }
+  toggleItalics(): void {
+    console.log(this.italics)
+    this.italics = !this.italics;
+  }
+
 }
